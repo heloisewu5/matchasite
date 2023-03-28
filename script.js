4a298dd9d419311dc25d54a1cfa652eab2ec31e2
test <pw2338@nyu.edu>
2023-03-28 10:25:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* First we get references to the elements on the page we want to interact with or manipulate */
+
+var menuButtonElement = document.getElementById("menuButton");
+var menuElement = document.getElementById("menu");
+var menuSectionElement = document.getElementById("menuSection");
+
+/* Then we "listen" for a click on the menuButton and toggle CSS classes to reveal the menu and invert the colors of the button+text */
+
+menuButtonElement.addEventListener('click', function(){
+	menuElement.classList.toggle('showMenu');
+	menuSection.classList.toggle('invertText');
+	menuButtonElement.classList.toggle('invertBG');
+});
